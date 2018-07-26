e0a3a53fb9374e6688d4d2f795b3102158c74eab
Devin Dow <DevinDow@gmail.com>
2018-07-26 16:22:41 +0000

    Given the following movies exist calls Movie.create!()

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+    puts "'#{movie["title"]}' was successfully created."
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
